780db7322aa7fcedc5d92afd2e41963569432ae8
bravo1goingdark <kumarashutosh34169@gmail.com>
2024-06-29 09:43:10 +0530

    leetcode 278

---
+++ badVersion.java
@@ -0,0 +1,17 @@
+public class badVersion {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left)/2;
+
+            if (isBadVersion(mid)) { // provide api 
+                right = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
