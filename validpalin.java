8db85235283fa2b200c58b1c17b1d4e16dd37dad
bravo1goingdark <kumarashutosh34169@gmail.com>
2024-07-22 13:30:42 +0530

    leetcode 125

--- validpalin.java
+++ validpalin.java
@@ -1,4 +1,4 @@
-
+// O(N) & O(N)
 public class validpalin {
     public boolean isPalindrome(String s) {
         Pair res = norm(s);
@@ -44,5 +44,33 @@ class Pair {
     }
     public String getStr(){
         return this.str;
+    }
+}
+
+// O(N) & O(1)
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
     }
 }
