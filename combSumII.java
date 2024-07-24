3b7ef34a51cb95aefcb76dadd9dd46a5595feba5
bravo1goingdark <kumarashutosh34169@gmail.com>
2024-07-24 11:45:59 +0530

    leetcode 40

--- combSumII.java
+++ combSumII.java
@@ -1,27 +1,33 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class combSumII {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         return findComb(0, candidates, target, new ArrayList<>());
     }
 
     private List<List<Integer>> findComb(int index, int[] arr, int target, List<Integer> res) {
         List<List<Integer>> ans = new ArrayList<>();
 
-        if (index == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(res));
-            }
-
+        if (target == 0) {
+            ans.add(new ArrayList<>(res));
             return ans;
         }
 
-        res.add(arr[index]);
-        ans.addAll(findComb(index + 1, arr, target - arr[index], res));
-        res.remove(res.size() - 1);
+        for (int ind = index; ind < arr.length; ind++) {
+            if (ind > index && arr[ind] == arr[ind - 1]) {
+                continue;
+            }
+            if (arr[ind] > target) {
+                break;
+            }
+            res.add(arr[ind]);
+            ans.addAll(findComb(ind + 1, arr, target - arr[ind], res));
+            res.remove(res.size() - 1);
+        }
 
-        ans.addAll(findComb(index + 1, arr, target, res));
         return ans;
     }
 }
